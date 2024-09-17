aef3ca3d6bbe1574bda7399e4472af65152af911
vishalsn33 <vishalsn33@gmail.com>
2024-09-17 22:30:26 +0530

    python file

---
+++ bb.py
@@ -0,0 +1,10 @@
+print("hello Vishal")
+n=10;
+print(n,"hello I am 10")
+print("So now we can perform")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+#   if x == "apple":
+#     continue
+  print(x)
